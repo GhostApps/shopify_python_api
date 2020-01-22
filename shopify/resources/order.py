ae733834b62444e3ca0ba29518fa1c0dfb45cd48
GhostApps <hello@ghostapps.co.uk>
2020-01-22 15:44:58 +0000

    Add support for fulfillment_order

--- shopify/resources/order.py
+++ shopify/resources/order.py
@@ -1,7 +1,7 @@
 from ..base import ShopifyResource
 from shopify import mixins
 from .transaction import Transaction
-
+from .fulfillment_order import FulfillmentOrder
 
 class Order(ShopifyResource, mixins.Metafields, mixins.Events):
 
@@ -19,3 +19,8 @@ class Order(ShopifyResource, mixins.Metafields, mixins.Events):
 
     def capture(self, amount=""):
         return Transaction.create({"amount": amount, "kind": "capture", "order_id": self.id})
+
+    def fulfillment_orders(self, **kwargs):
+        return FulfillmentOrder.find(from_="%s/orders/%s/fulfillment_orders.json" % (
+            ShopifyResource.site, self.id), **kwargs)
+
